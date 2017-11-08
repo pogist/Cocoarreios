e9d7d12aab8ed0923fd7d0d81fbc797808430550
pogist <murilo.paixao.2@gmail.com>
2017-11-08 12:45:16 -0300

    (refactor): Improved naming

--- Cocoarreios/Classes/Address.swift
+++ Cocoarreios/Classes/Address.swift
@@ -8,15 +8,15 @@
 import Foundation
 
 public struct Address: Codable {
-    let cep: String
-    let publicPlace: String
-    let complement: String
-    let neighborhood: String
-    let city: String
-    let state: String
-    let unity: String
-    let ibge: String
-    let gia: String
+    public let cep: String
+    public let publicPlace: String
+    public let complement: String
+    public let neighborhood: String
+    public let city: String
+    public let state: String
+    public let unity: String
+    public let ibge: String
+    public let gia: String
     
     enum CodingKeys: String, CodingKey {
         case cep
--- Cocoarreios/Classes/CocoarreiosTarget.swift
+++ Cocoarreios/Classes/CocoarreiosTarget.swift
@@ -3,42 +3,42 @@ import Moya
 import Alamofire
 
 public enum CocoarreiosTarget {
-    case searchAddress(code: String)
+    case address(cep: String)
 }
 
 extension CocoarreiosTarget: TargetType {
     
     public var baseURL: URL {
         switch self {
-        case .searchAddress(_):
+        case .address(_):
             return URL(string: "https://viacep.com.br/ws")!
         }
     }
     
     public var path: String {
         switch self {
-        case .searchAddress(let code):
-            return "/\(code)/json/"
+        case .address(let cep):
+            return "/\(cep)/json/"
         }
     }
     
     public var method: Moya.Method {
         switch self {
-        case .searchAddress(_):
+        case .address(_):
             return .get
         }
     }
     
     public var task: Task {
         switch self {
-        case .searchAddress(_):
+        case .address(_):
             return .requestPlain
         }
     }
     
     public var sampleData: Data {
         switch self {
-        case .searchAddress(_):
+        case .address(_):
             return """
             {
                 "cep": "01001-000",
@@ -58,7 +58,7 @@ extension CocoarreiosTarget: TargetType {
     public var headers: [String: String]? {
         switch self {
             
-        case .searchAddress(_):
+        case .address(_):
             return [
                 "Content-Type": "application/json",
                 "Accept": "application/json"
