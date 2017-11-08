40670f2e5dcd5e7f0ba0c9f8317cead2443982b8
pogist <murilo.paixao.2@gmail.com>
2017-11-08 12:45:49 -0300

    (refactor): Added a better example

--- Example/Cocoarreios/ViewController.swift
+++ Example/Cocoarreios/ViewController.swift
@@ -14,12 +14,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        Cocoarreios.request(.searchAddress(code: "60861190")) { result in
+        Cocoarreios.request(.address(cep: "01001000")) { result in
             switch result {
             case .success(let address):
-                print(address)
+                print(address.cep)          // => "01001-000"
+                print(address.publicPlace)  // => "Praça da Sé"
+                print(address.complement)   // => "lado ímpar"
+                print(address.neighborhood) // => "Sé"
+                print(address.city)         // => "São Paulo"
+                print(address.state)        // => "SP"
+                print(address.unity)        // => ""
+                print(address.ibge)         // => "3550308"
+                print(address.gia)          // => "1004"
                 
             case .failure(let error):
+                //Do something with error...
                 print(error)
             }
         }
